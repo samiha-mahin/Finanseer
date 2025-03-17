9fdd9616f4d590e0b0ecb60705d4fdd738996aee
samiha-mahin <samihamahin36@gmail.com>
2025-03-17 23:00:30 +0600

    f

--- client/src/scenes/dashboard/Row1.jsx
+++ client/src/scenes/dashboard/Row1.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Row1 = () => {
   return (
     <>
-      <Card className="bg-zinc-700 p-4 border-0" style={{ gridArea: 'a' }}>Box A</Card>
+      <Card className="bg-zinc-700 p-4 border-0 shadow-xl shadow-black/70 drop-shadow-2xl" style={{ gridArea: 'a' }}>Box A</Card>
       <Card className="bg-zinc-700 p-4 border-0" style={{ gridArea: 'b' }}>Box B</Card>
       <Card className="bg-zinc-700 p-4 border-0" style={{ gridArea: 'c' }}>Box C</Card>
     </>
