a76decab027a7b05667f95ec44923bda1f42e065
samiha-mahin <samihamahin36@gmail.com>
2025-03-17 23:01:41 +0600

    f

--- client/src/scenes/dashboard/Row1.jsx
+++ client/src/scenes/dashboard/Row1.jsx
@@ -5,8 +5,8 @@ const Row1 = () => {
   return (
     <>
       <Card className="bg-zinc-700 p-4 border-0 shadow-xl shadow-black/70 drop-shadow-2xl" style={{ gridArea: 'a' }}>Box A</Card>
-      <Card className="bg-zinc-700 p-4 border-0" style={{ gridArea: 'b' }}>Box B</Card>
-      <Card className="bg-zinc-700 p-4 border-0" style={{ gridArea: 'c' }}>Box C</Card>
+      <Card className="bg-zinc-700 p-4 border-0 shadow-xl shadow-black/70 drop-shadow-2xl" style={{ gridArea: 'b' }}>Box B</Card>
+      <Card className="bg-zinc-700 p-4 border-0 shadow-xl shadow-black/70 drop-shadow-2xl" style={{ gridArea: 'c' }}>Box C</Card>
     </>
   );
 };
