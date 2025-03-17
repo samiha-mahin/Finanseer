361ebb5b326f9a829fd07d7dfab84fcf16f52320
samiha-mahin <samihamahin36@gmail.com>
2025-03-18 02:10:17 +0600

    finanseer

--- client/src/scenes/dashboard/Dashboard.jsx
+++ client/src/scenes/dashboard/Dashboard.jsx
@@ -1,37 +1,71 @@
-import React from "react";
+import { useMediaQuery } from "react-responsive";
 import Row1 from "./Row1";
 import Row2 from "./Row2";
 import Row3 from "./Row3";
 
+const gridTemplateLargeScreens = `
+  "a b c"
+  "a b c"
+  "a b c"
+  "a b f"
+  "d e f"
+  "d e f"
+  "d h i"
+  "g h i"
+  "g h j"
+  "g h j"
+`;
+
+const gridTemplateSmallScreens = `
+  "a"
+  "a"
+  "a"
+  "a"
+  "b"
+  "b"
+  "b"
+  "b"
+  "c"
+  "c"
+  "d"
+  "d"
+  "d"
+  "e"
+  "e"
+  "f"
+  "f"
+  "g"
+  "g"
+  "g"
+  "h"
+  "h"
+  "h"
+  "h"
+  "i"
+  "i"
+  "j"
+  "j"
+`;
+
 const Dashboard = () => {
+  const isAboveMediumScreens = useMediaQuery({ minWidth: 1200 });
+
   return (
     <div
-      className="w-full h-full grid gap-6 p-4
-        sm:grid-cols-1 sm:grid-rows-[repeat(24,_80px)] 
-        md:grid-cols-1 md:grid-rows-[repeat(24,_80px)] 
-        lg:grid-cols-3 lg:grid-rows-[repeat(10,_minmax(60px,_1fr))] 
-        xl:grid-cols-3 xl:grid-rows-[repeat(10,_minmax(60px,_1fr))] 
-        2xl:grid-cols-3 2xl:grid-rows-[repeat(10,_minmax(60px,_1fr))]"
+      className={`w-full h-full grid gap-6 p-4 ${
+        isAboveMediumScreens
+          ? "grid-cols-3 grid-rows-[repeat(10,_minmax(60px,_1fr))]"
+          : "grid-cols-1 auto-rows-[80px]"
+      }`}
       style={{
-        gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
-        gridTemplateRows: "repeat(10, minmax(60px, 1fr))",
-        gridTemplateAreas: `
-         "a b c"
-         "a b c"
-         "a b c"
-         "a b f"
-         "d e f"
-         "d e f"
-         "d h i"
-         "g h i"
-         "g h j"
-         "g h j"
-        `,
+        gridTemplateAreas: isAboveMediumScreens
+          ? gridTemplateLargeScreens
+          : gridTemplateSmallScreens,
       }}
     >
-      <Row1 />
-      <Row2 />
-      <Row3 />
+      <Row1/>
+      <Row2/>
+      <Row3/>
     </div>
   );
 };
