bae0270028dc59d8c9f84f3059d66da1b79c7ed5
samiha-mahin <samihamahin36@gmail.com>
2025-03-18 00:54:59 +0600

    finanseer

--- client/src/scenes/dashboard/Row1.jsx
+++ client/src/scenes/dashboard/Row1.jsx
@@ -1,7 +1,9 @@
 import { Card } from '@/components/ui/card';
+import { useGetKpisQuery } from '@/state/api';
 import React from 'react';
 
 const Row1 = () => {
+    const {data} = useGetKpisQuery();
   return (
     <>
       <Card className="bg-zinc-800 p-4 border-0 shadow-xl shadow-black/70 drop-shadow-2xl" style={{ gridArea: 'a' }}>Box A</Card>
