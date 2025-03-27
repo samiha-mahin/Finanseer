f3dd0bcbf23f884e8d1f42c70f3675eae47ee60e
samiha-mahin <samihamahin36@gmail.com>
2025-03-28 00:14:14 +0600

    f

--- client/src/scenes/Predictions.jsx
+++ client/src/scenes/Predictions.jsx
@@ -1,8 +1,15 @@
+import { Card } from '@/components/ui/card'
 import React from 'react'
 
 const Predictions = () => {
   return (
-    <div>Predictions</div>
+    <>
+    <Card
+        className="w-full h-full bg-zinc-800 p-4 border-0 shadow-xl shadow-black/70 drop-shadow-2xl"
+      >
+        <h1 className="text-white text-2xl">Predictions</h1>
+      </Card>
+    </>
   )
 }
 
