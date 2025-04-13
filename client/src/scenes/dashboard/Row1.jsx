2a286dc5f45b0c381712d1feaf8b86327723b045
samiha-mahin <samihamahin36@gmail.com>
2025-04-13 22:25:33 +0600

    finanseer

--- client/src/scenes/dashboard/Row1.jsx
+++ client/src/scenes/dashboard/Row1.jsx
@@ -81,7 +81,7 @@ const Row1 = () => {
         <ResponsiveContainer width="100%" height="100%">
           <AreaChart
             data={revenueExpenses}
-            margin={{ top: 15, right: 25, left: -10, bottom: 40 }}
+            margin={{ top: 15, right: 25, left: -10, bottom:40 }}
           >
             <defs>
               <linearGradient id="colorRevenue" x1="0" y1="0" x2="0" y2="1">
